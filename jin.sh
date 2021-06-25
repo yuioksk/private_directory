9be4daae37d93c5a44f4386fc917e5df047454d5
saranya <saran@gmail.com>
2021-06-25 09:33:58 +0530

    my first commit

---
+++ jin.sh
@@ -0,0 +1,25 @@
+src="/home/e1027/manbkp/san.txt"
+targ="/opt/backup/"
+
+if [ ! -f $src ]
+then
+	echo "File $src does not exists"
+	exit 1
+elif [ -f $targ ]
+then
+	echo "File $targ exist, cannot overwrite"
+	exit 2
+fi
+ 
+# copy file 
+sudo cp $src $targ
+ 
+status=$?
+ 
+if [ $status -eq 0 ]
+then
+	echo 'File copied successfully'
+else
+	echo 'Problem copuing file'
+fi
+
